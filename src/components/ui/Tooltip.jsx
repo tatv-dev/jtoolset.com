5780fe8668909379a009a374961ebd82b47007c4
DESKTOP-KLO8Q3T\trinh <trinhvanta1804@gmail.com>
2025-05-15 16:33:27 +0700

    fix tooltip

---
+++ src/components/ui/Tooltip.jsx
@@ -0,0 +1,22 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+
+export function Tooltip({ children, content, side = 'top' }) {
+  return (
+    <TooltipPrimitive.Provider>
+      <TooltipPrimitive.Root>
+        <TooltipPrimitive.Trigger asChild>
+          {children}
+        </TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Portal>
+          <TooltipPrimitive.Content
+            side={side}
+            className="z-50 rounded bg-black px-2 py-1 text-xs text-white shadow-md"
+          >
+            {content}
+            <TooltipPrimitive.Arrow className="fill-black" />
+          </TooltipPrimitive.Content>
+        </TooltipPrimitive.Portal>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
+  );
+}
